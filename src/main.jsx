8b993aec13777b08a0d9f20e9b187cd9224c00e1
Kevyn lucca <Kevynalternativo@gmail.com>
2024-07-05 19:17:13 -0300

    Começo do carrinho de compras

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-
 import { ChakraProvider } from "@chakra-ui/react";
 import CartProvider from "./ProductCartContext.jsx";
 
@@ -15,3 +14,13 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 		</ChakraProvider>
 	</React.StrictMode>
 );
+/*
+	Todos:
+	estilizar o carrinho
+	estilazar os carroseis de produtos
+	criar paginas de empresas
+	criar rotas a partir das tag (a logica de busca por item especifico pode ser a mesma de product page)
+	criar o componente product page, um componente gerado a partir das rotas (o componente deve ser criado a partir do id do produto)
+	criar um carrosel para screenshots do jogo para o componente "Product page"
+
+*/
